97cd5ae7f6d011b03806af85cfd8ff0c96cf4879
angello <angello@fractalprotocol.org>
2024-10-31 10:55:41 -0400

    updated favicons and open graph images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,16 @@ import { dinCondensed } from "@/config/fonts";
 export const metadata: Metadata = {
   title: "Restricted | Upshift",
   description: "Earn yields from real institutional loans via the Upshift protocol. Democratizing high-yield investments traditionally limited to financial institutions.",
+  openGraph: {
+    type: "website",
+    url: "https://terms.upshift.finance",
+    title: "Upshift Finance",
+    description: "Earn yields from real institutional loans via the Upshift protocol. Democratizing high-yield investments traditionally limited to financial institutions.",
+    siteName: "Upshift Finance",
+    images: [{
+      url: "https://terms.upshift.finance/og-image.jpg",
+    }],
+  }
 };
 
 export default function RootLayout({
